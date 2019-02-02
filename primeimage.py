eaaf9a7b6aff0885063765c7cf874f5a8ea649a6
WillMatthews <will.a.matthews@me.com>
2019-02-02 02:25:42 +0000

    all ints 0-9 now used

--- primeimage.py
+++ primeimage.py
@@ -1,15 +1,18 @@
+#!/usr/bin/python3
 # written 3 oct 18 by William Matthews
 # inspired by the Trinity Hall Prime
+
+#TODO add histogram equalisation?
+#TODO add a 'force binary' option
+#TODO add a 'abberate digits' option
 
 from scipy import misc
 import numpy as np
 import time
 
-#import matplotlib.pyplot as plt
-
 import getopt
 import sys
-
+import math
 
 
 def _try_composite(a, d, n, s):
@@ -31,24 +34,24 @@ def is_prime(n, _precision_for_huge_n=16):
     while not d % 2:
         d, s = d >> 1, s + 1
     # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
-    if n < 1373653: 
+    if n < 1373653:
         return not any(_try_composite(a, d, n, s) for a in (2, 3))
-    if n < 25326001: 
+    if n < 25326001:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
-    if n < 118670087467: 
-        if n == 3215031751: 
+    if n < 118670087467:
+        if n == 3215031751:
             return False
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
-    if n < 2152302898747: 
+    if n < 2152302898747:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
-    if n < 3474749660383: 
+    if n < 3474749660383:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
-    if n < 341550071728321: 
+    if n < 341550071728321:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
     # otherwise
-    return not any(_try_composite(a, d, n, s) 
+    return not any(_try_composite(a, d, n, s)
                    for a in _known_primes[:_precision_for_huge_n])
- 
+
 
 def primecheckrange(startnum,numtotest=10000):
     """Given a starting number (the image), increments image until a prime
@@ -72,43 +75,42 @@ def primecheckrange(startnum,numtotest=10000):
         return int()
 
 
-def to_binary(img, lower, upper):
-    """Converts numpy ndarray from integer to Boolean
-    This is useful as a greyscale can be fed in and a binary image returned"""
-    return (lower< img) & (img < upper)
-
-
-def to_imagenum(pixel, white=1, black=8):
-    """Converts Boolean pixel to an integer"""
-    if pixel:
-        return white
-    else:
-        return black
-
-
 def get_image(path,size_y=25,size_x=50,autoaspect=True):
     """Given the path of an image, function reads image, resizes, and returns a binary image with dimensions in a tuple"""
     # read image
     f = misc.imread(path, mode='L') # read as greyscale
-    
+
     if autoaspect:
         # get aspect ratio, y/x
         ar = len(f)/len(f[0])
         pass # feature not yet implemented
 
     f = misc.imresize(f,(size_y, size_x),interp='bilinear') # resize as a 25 x 50
-    bw = to_binary(f,150,256)
-    return (bw,(size_y, size_x))
+    #bw = to_binary(f,150,256)
+    return (f,(size_y, size_x))
 
 
-def get_imagenum(binimage):
+def get_imagenum(image):
     """get_imagenum takes a binary image (numpy ndarray of Bool) and converts it to a single integer of numbers as specified in to_imagenum"""
 
     # concat list
     imgnumlist = []
-    for row in binimage:
-        for element in row:
-            imgnumlist.append(to_imagenum(element))
+    pixelset = [8,9,6,5,4,3,0,2,7,1]
+
+    bounds = []
+
+    for i, waste in enumerate(pixelset):
+        bounds.append(i * (255/len(pixelset)-10))
+
+    bounds = [math.floor(bound) for bound in bounds]
+    bounds.append(256)
+
+    for row in image:
+        for pixel in row:
+            for i, pixelset_item in enumerate(pixelset):
+                if (bounds[i] <= pixel) & (pixel < bounds[i+1]):
+                    imgnumlist.append(pixelset_item)
+
     s = ''.join(map(str,imgnumlist))
     print("Image is", len(s) ," digits long")
     return int(s)
@@ -155,16 +157,13 @@ def univcrest():
 
 #######################################################
 
-
-
 # build list of prime numbers for primality test to use
 _known_primes = [2, 3]
 _known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
 
 
-
 def main(argv):
-    
+
     # parse arguments
     useuniv = False
     path = ""
@@ -176,7 +175,7 @@ def main(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print("\nprimeimage.py -h -i <inputfile> -u \n\n -h :  prints help \n -i : input file \n -u : load University College crest")
+            print("\nprimeimage.py -h -i <inputfile> -u \n\n -h : prints help \n -i : input file \n -u : load University College crest")
             sys.exit()
         elif opt in ("-u","--univ"):
             useuniv = True
@@ -189,8 +188,8 @@ def main(argv):
     else:
         # if we have a path to work with
         if path:
-            bimage, size = get_image(path,size_x=50,size_y=25)
-            imagenum = get_imagenum(bimage)
+            image, size = get_image(path,size_x=50,size_y=25)
+            imagenum = get_imagenum(image)
         else:
             print("No Image Specified!")
             imagenum = 0
@@ -224,7 +223,6 @@ def main(argv):
         print("Prime Not Found, Expand search and try again")
 
     print("Elapsed Time:", t1-t0 ,"seconds")
-
 
 
 if __name__ == "__main__":
